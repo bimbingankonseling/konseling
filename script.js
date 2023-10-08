31aeef3c87448ea2a602d9f4a3fe60549c642a1b
Rolly Maulana Awangga <rolly@awang.ga>
2023-10-08 16:40:50 +0700

    fix

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('proposal').addEventListener('click', function() {
+    // Arahkan pengguna ke proposal.html
+    window.location.href = 'proposal.html';
+});
---
+++ tampilan.js
@@ -0,0 +1,22 @@
+import data from "./json/tampilan.json" assert { type: "json" };
+import { setInner } from "https://jscroot.github.io/element/croot.js";
+
+setInner("project", data.project);
+setInner("nama", data.nama);
+setInner("judulabout", data.judulabout);
+setInner("subjudulabout", data.subjudulabout);
+setInner("isiabout", data.isiabout);
+setInner("isi1", data.isi1);
+setInner("isi2", data.isi2);
+setInner("isi3", data.isi3);
+setInner("isi4", data.isi4);
+setInner("isi5", data.isi5);
+setInner("isi6", data.isi6);
+setInner("judulview", data.judulview);
+setInner("view", data.view);
+setInner("judulteam", data.judulteam);
+setInner("namadipa", data.namadipa);
+setInner("npmdipa", data.npmdipa);
+setInner("bella", data.bella);
+setInner("npmbel", data.npmbel);
+setInner("button", data.button);
